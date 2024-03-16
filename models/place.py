fc1c6d794a689c934f4443313fb735e5dabbac1a
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-03-16 19:41:22 +0000

    Adding new file

--- models/place.py
+++ models/place.py
@@ -4,6 +4,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,14 +29,6 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship('Review', backref='place',
                            cascade='all, delete-orphan')
-    place_amenity = Table('place_amenity',
-                          Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'), primary_key=True,
-                                 nullable=False),
-                          Column('amenity.id', String(60),
-                                 ForeignKey('amenities.id'), primary_key=True,
-                                 nullable=False))
     amenities = relationship('Amenity', secondary=place_amenity,
-                             viewonly=False)
+                             back_populates='place_amenities', viewonly=False)
     amenity_ids = []
