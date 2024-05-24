afb5fb0d02b8efd1b5edd01ff522816c04f3a6a7
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-05-24 13:23:13 +0000

    Adding new file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -34,9 +34,28 @@ creating_data() {
         sudo rm /data/web_static/current
     fi
     sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-    
-    sudo sed -i "59i\
-    \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm\n\t}" /etc/nginx/sites-available/default
+
+    printf %s "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root   /var/www/html;
+        index  index.html index.htm;
+        
+        location /hbnb_static {
+            alias /data/web_static/current;
+            index index.html index.htm;
+        }
+
+        location /redirect_me {
+            return 301 http://youtube.com/;
+        }
+
+        error_page 404 /404.html;
+        location /404 {
+            root /var/www/html;
+            internal;
+        }
+    }" > /etc/nginx/sites-available/default
 }
 
 # Checking if nginx is installed or not
