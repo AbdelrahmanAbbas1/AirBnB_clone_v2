71bef41eed013187c90925874fb7d4e8c42d55f3
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-05-24 13:26:43 +0000

    Adding new file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -29,11 +29,12 @@ creating_data() {
         sudo touch  /data/web_static/releases/test/index.html
         echo "Hello this is just a test" | sudo tee /data/web_static/releases/test/index.html > /dev/null
     fi
-    sudo chown -R ubuntu:ubuntu /data/
     if [ -L /data/web_static/current ]; then
         sudo rm /data/web_static/current
     fi
     sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+    sudo chown -R ubuntu:ubuntu /data/
 
     printf %s "server {
         listen 80 default_server;
