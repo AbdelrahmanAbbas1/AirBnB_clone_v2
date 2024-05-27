5cb272dbd1428b0fac55b21f279dfc5c00e16f74
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-05-27 00:34:33 +0000

    Adding new file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,20 +5,13 @@ from fabric.api import *
 do_pack = __import__("1-pack_web_static").do_pack
 do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
-# Variable to keep track of the function calls
-archive = None
 env.hosts = ["100.25.181.194", "54.237.49.41"]
 
 
 def deploy():
     """This function depolys an archive to servers"""
-    global archive
-    # Ensure do_pack is called only once
-    if archive is None:
-        archive = do_pack()
-        if archive is None:
-            return False
-    # Store the archive created by do_pack because it will be only called once
-    archive_path = archive
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
     res = do_deploy(archive_path)
     return res
