e3309af65d2b09213f1aa82439239cba4353b7e6
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-08-14 02:08:05 +0000

    Adding new file

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage
 
 
 class State(BaseModel, Base):
@@ -11,3 +12,17 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state',
                           cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        all_states = storage.all()
+        l_cities = []
+        l_common = []
+        for key in all_states:
+            city = key.split('.')
+            if city[0] == 'City':
+                l_cities.append(all_states[key])
+        for el in l_cities:
+            if el.state_id == self.id:
+                l_common.append(el)
+        return l_common
